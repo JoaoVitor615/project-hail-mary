654ea43d53eb2af9d94aef92a86a05e748c34c2c
JoaoVitor615 <jvpeniza@gmail.com>
2026-03-18 08:35:57 -0300

    Add NASA's JPL Horizons IDs and API URL to constants.h

--- include/constants.h
+++ include/constants.h
@@ -14,4 +14,14 @@
 // Color Pairs
 #define COLOR_PAIR_PRIMARY 1
 
+// NASA's JPL Horizons IDs
+#define ID_SUN     "@sun"
+#define ID_MERCURY "199"
+#define ID_VENUS   "299"
+#define ID_EARTH   "399"
+#define ID_MARS    "499"
+
+// BASE API URL for NASA's JPL Horizons
+#define HORIZONS_API_URL "https://ssd.jpl.nasa.gov/api/horizons.api"
+
 #endif
