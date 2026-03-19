dde17674cc6b386262847a12fd43ebdff51268b7
JoaoVitor615 <jvpeniza@gmail.com>
2026-03-19 20:38:38 -0300

    Refactor get_coordenates function prototype and define Vector2D struct in nasa_api.h

--- include/nasa_api.h
+++ include/nasa_api.h
@@ -1,1 +1,11 @@
-void get_coordenates(const char* planet_id, const char* center, char* output_buffer);
+#ifndef NASA_API_H
+#define NASA_API_H
+
+typedef struct {
+    double x;
+    double y;
+} Vector2D;
+
+Vector2D get_coordenates(const char* planet_id, const char* center);
+
+#endif
