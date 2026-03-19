6b1298f5295a7ef09c65ce321557969f495568c5
JoaoVitor615 <jvpeniza@gmail.com>
2026-03-19 20:38:53 -0300

    Refactor main function to utilize Vector2D for solar system coordinates and remove unused output buffer

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <curl/curl.h>
 #include "../include/intro.h"
 #include "../include/nasa_api.h"
+#include "constants.h"
 
 int main() {
     initscr();
@@ -11,9 +12,19 @@ int main() {
 
     play_intro();
 
-    char output_buffer[8192] = {0};
-    get_coordenates("399", "@sun", output_buffer);
-
     endwin();
+
+    Vector2D solarSystemCoords[5] = { 0 };
+
+    solarSystemCoords[0] = get_coordenates(ID_SUN, ID_SUN);
+    solarSystemCoords[1] = get_coordenates(ID_MERCURY, ID_SUN);
+    solarSystemCoords[2] = get_coordenates(ID_VENUS, ID_SUN);
+    solarSystemCoords[3] = get_coordenates(ID_EARTH, ID_SUN);
+    solarSystemCoords[4] = get_coordenates(ID_MARS, ID_SUN);
+
+    for (int i = 0; i < 5; i++) {
+        printf("Planet %d - X: %f, Y: %f\n", i, solarSystemCoords[i].x, solarSystemCoords[i].y);
+    }
+
     return 0;
 }
